b8d7ef9b8a14232f67bc28873e5d82f648317b15
Nathan <nathancai1031@gmail.com>
2021-02-24 12:13:08 -0500

    added option to add attendees

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import EventForm from './components/EventForm';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <EventForm />
   </React.StrictMode>,
   document.getElementById('root')
 );
