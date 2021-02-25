f0121d9ca035780ee2ce8c69060b6d6b02e0060a
Nathan <nathancai1031@gmail.com>
2021-02-25 10:44:43 -0500

    wrote 3 test cases

---
+++ src/__tests__/components/ParticipantsForms.test.js
@@ -0,0 +1,36 @@
+import React from "react";
+import {shallow} from "enzyme";
+import EventForm from "../../components/EventForm";
+
+let wrapper, summaryChange, descChange;
+
+beforeEach(() => {
+    summaryChange = jest.fn();
+    descChange = jest.fn();
+
+    wrapper = shallow(
+        <EventForm 
+            summaryChange={summaryChange}
+            descChange={descChange}
+            start={new Date("Thu Feb 25 2021 10:42:04 GMT-0500 (Eastern Standard Time)")}
+        />
+    );
+});
+
+test("Should render component", () => {
+    expect(wrapper).toMatchSnapshot();
+});
+
+test("Should handle summary change", () => {
+    wrapper.find("#summary").simulate("change", {
+        target: {value: "Hello World"}
+    });
+    expect(summaryChange).toHaveBeenCalledWith("Hello World");
+});
+
+test("Should handle description change", () => {
+    wrapper.find("#description").simulate("change", {
+        target: {value: "Hello World! Visit us!"}
+    });
+    expect(descChange).toHaveBeenCalledWith("Hello World! Visit us!");
+});
